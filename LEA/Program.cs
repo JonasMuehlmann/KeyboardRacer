4c65e9e789d9a37407cbc3cb8a31c5ad5782bb0a
HollowProphet <49788305+HollowProphet@users.noreply.github.com>
2020-09-09 12:55:07 +0200

    Small tests.

--- LEA/Program.cs
+++ LEA/Program.cs
@@ -9,11 +9,11 @@ namespace LEA
     {
         private static void Main(string[] args)
         {
-            var frametime = 1000 / 144 / 2;
+            const int frametime = 1000 / 144 / 2;
 
-                var car = @"         ¸______¸
+                const string car = @"         ¸______¸
         ɍ___ǁ____ƪ___
-        ¬(˽)----¬(˽)-' ";
+        ¬(˽)----¬(˽)-'";
 
             // for (var i = 0; i < 100; i++)
             for (var i = 0; i < 1; i++)
@@ -31,11 +31,11 @@ namespace LEA
                 Thread.Sleep(frametime * 5);
                 Console.Clear();
                 Console.WriteLine("\x1b[38;2;<255>;<125>;<0>mFoo");
-                Console.WriteLine("\x1b[48;2;<0>;<125>;<255>mBar");
+                Console.WriteLine("\x1b[51;2;<0>;<125>;<255>mBar");
             }
 
             // https://notes.burke.libbey.me/ansi-escape-codes/
-            // \x1b or \e  or \033 gibt Folgen von escape sequence an
+            // \x1b or \e  or \u001b or \033 gibt Folgen von escape sequence an
             Console.Write("\x1b[36mTEST\x1b[0m");
             Thread.Sleep(800);
             // Back to beginning
@@ -51,6 +51,11 @@ namespace LEA
             Console.SetCursorPosition(0, 3);
             Console.Write("abcd");
             Console.SetCursorPosition(0, oldLine);
+            Console.WriteLine($"{Effects.Bold}{Effects.Underline}{Effects.Italic}{FgBright.Red}{Bg.Blue}Bar{Fg.Reset}{Bg.Reset}{Effects.Reset}Bar");
+            Console.WriteLine($"{Effects.Bold}Foo{Effects.Reset}");
+            Console.WriteLine($"{Effects.Reverse}Foo{Effects.Reset}");
+            Console.WriteLine($"{Effects.Italic}Foo{Effects.Reset}");
+            Console.WriteLine($"{Effects.Underline}Foo{Effects.Reset}");
         }
     }
 }
