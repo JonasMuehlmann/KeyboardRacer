d2efe51c587e86c0a1d8afe0a72804a852fbd6ad
assaro <washambala@yahoo.de>
2020-09-14 15:17:11 +0200

    Added Text.cs Added TODO on Text.cs

---
+++ LEA/Text.cs
@@ -0,0 +1,21 @@
+namespace LEA
+
+
+{
+    using Microsoft.VisualBasic.FileIO;
+    public class Text
+    {
+        private int Id { get; set; } 
+        private string Phrase { get; set; }
+     
+        
+        //TODO: Constructor
+        //TODO: Load Phrase based on ID
+        //TODO: Load Phrase based on Difficulty
+
+
+
+
+
+    }
+}
