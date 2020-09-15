01ee907a9edfd76adf992fc36b74d56d3e12e75e
HollowProphet <49788305+HollowProphet@users.noreply.github.com>
2020-09-15 14:17:08 +0200

    Added variable Car and CarHeight to class Constants Added classes "Cursor" and "Line" to Escape_sequences.cs

--- LEA/Escape_sequences.cs
+++ LEA/Escape_sequences.cs
@@ -3,6 +3,12 @@ namespace LEA
     public static class Constants
     {
         public const string Csi = "\x1b[";
+
+        public const string Car = @"         ¸______¸
+        ɍ___ǁ____ƪ___
+        ¬(˽)----¬(˽)-'";
+
+        public const int CarHeight = 4;
     }
 
     public static class Fg
@@ -74,7 +80,104 @@ namespace LEA
         public static readonly string Overline   = $"{Constants.Csi}53m";
         public static readonly string NoOverline = $"{Constants.Csi}55m";
 
-        public static readonly string Blink = $"{Constants.Csi}5m";
-        public static readonly string NoBlink   = $"{Constants.Csi}25m";
+        public static readonly string Blink   = $"{Constants.Csi}5m";
+        public static readonly string NoBlink = $"{Constants.Csi}25m";
+    }
+
+    public static class Cursor
+    {
+        /// <summary>
+        /// Represents escape code to move cursor up n lines.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static string Up(int n)
+        {
+            return $"{Constants.Csi}{n}A";
+        }
+
+
+        /// <summary>
+        /// Represents escape code to move cursor down n lines.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static string Down(int n)
+        {
+            return $"{Constants.Csi}{n}B";
+        }
+
+
+        /// <summary>
+        /// Represents escape code to move cursor left n column.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static string Left(int n)
+        {
+            return $"{Constants.Csi}{n}D";
+        }
+
+
+        /// <summary>
+        /// Represents escape code to move cursor right n columns.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static string Right(int n)
+        {
+            return $"{Constants.Csi}{n}C";
+        }
+
+
+        /// <summary>
+        /// Move Cursor to the n'th column in the current line
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static string ToCol(int n)
+        {
+            return $"{Constants.Csi}{n}G";
+        }
+
+
+        /// <summary>
+        /// Move to cursor to the position (row,column)
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="col"></param>
+        /// <returns></returns>
+        public static string To(int row, int col)
+        {
+            return $"{Constants.Csi}{row};{col}H";
+        }
+    }
+
+    public static class Line
+    {
+        public static string EraseWhole()
+        {
+            return $"{Constants.Csi}2K";
+        }
+
+
+        /// <summary>
+        /// Erase from the beginning of the line to the current cursor position
+        /// </summary>
+        /// <returns></returns>
+        public static string EraseToCursors()
+        {
+            return $"{Constants.Csi}1K";
+        }
+
+
+        /// <summary>
+        /// Erase from the current cursor position the the end of the line
+        /// </summary>
+        /// <returns></returns>
+        public static string EraseFromCursors()
+        {
+            return $"{Constants.Csi}0K";
+        }
     }
 }
