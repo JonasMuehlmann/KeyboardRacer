147c94a99b34a09cb5ac6fcf8203ef6068419599
HollowProphet <49788305+HollowProphet@users.noreply.github.com>
2020-09-19 12:36:23 +0200

    Removed commented out

--- LEA/Program.cs
+++ LEA/Program.cs
@@ -97,35 +97,7 @@
 
             #endregion RenderTest
 
-            /*
-            Console.WriteLine("\x1b[38;2;<255>;<125>;<0>mFoo");
-            Console.WriteLine("\x1b[51;2;<0>;<125>;<255>mBar");
-            // https://notes.burke.libbey.me/ansi-escape-codes/
-            // \x1b or \e  or \u001b or \033 gibt Folgen von escape sequence an
-            Console.Write("\x1b[36mTEST\x1b[0m");
-            Thread.Sleep(800);
-            // Back to beginning
-            Console.Write("\r");
-            // Go back one char
-            // Console.Write("\b");
-            Console.WriteLine("fooo");
-            Console.WriteLine("baar");
-            Console.WriteLine(Console.BufferHeight);
-            int oldLine = Console.BufferHeight;
-            Console.WriteLine(Console.BufferWidth);
-            Thread.Sleep(800);
-            Console.SetCursorPosition(0, 3);
-            Console.Write("abcd");
-            Console.SetCursorPosition(0, oldLine);
 
-            Console.WriteLine($"{Effects.Bold}{Effects.Underline}{Effects.Italic}{Fg.BrightRed}{Bg.Blue}Bar{Fg.Reset}{Bg.Reset}{Effects.Reset}Bar"
-                             );
-
-            Console.WriteLine($"{Effects.Bold}Foo{Effects.Reset}");
-            Console.WriteLine($"{Effects.Reverse}Foo{Effects.Reset}");
-            Console.WriteLine($"{Effects.Italic}Foo{Effects.Reset}");
-            Console.WriteLine($"{Effects.Underline}Foo{Effects.Reset}");
-            */
             var text =
                 "Lorem ipsum dolor sit amet.";
 
@@ -134,9 +106,9 @@
             Participant[] players =
             {
                 new Player("Foo", Fg.Magenta, race),
-                new Bot("Bot1", Fg.Blue,  race, 5),
-                new Bot("Bot2", Fg.Red,   race, 6),
-                new Bot("Bot3", Fg.Green, race, 7)
+                // new Bot("Bot1", Fg.Blue,  race, 5),
+                // new Bot("Bot2", Fg.Red,   race, 6),
+                // new Bot("Bot3", Fg.Green, race, 7)
             };
 
             race.Participants.AddRange(players);
